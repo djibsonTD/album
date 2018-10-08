a35330f73cf640311b642f1e92471de8d38654d5
goclasse <tyerasouleyman@gmail.com>
2018-10-08 14:40:16 +0200

    Installation et style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,10 +69,10 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">@lang('Accueil')</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('login') }}">@lang('Connexion')</a>
+                        <a href="{{ route('register') }}">@lang('Inscription')</a>
                     @endauth
                 </div>
             @endif
